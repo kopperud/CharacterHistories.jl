9051c939b63fe1d34d763073277d33da223e2691
Bjorn Tore Kopperud <kopperud@protonmail.com>
2024-04-03 16:16:22 +0200

    add vertical lines to plot

--- ext/CharacterHistoriesMakieExt.jl
+++ ext/CharacterHistoriesMakieExt.jl
@@ -5,29 +5,48 @@ import Makie, CharacterHistories
 function CharacterHistories.treeplot(
     tree::CharacterHistories.Root
 )
+
     fig = Makie.Figure()
     ax = Makie.Axis(fig[1,1], xreversed = true)
 
     Makie.hidespines!(ax)
     Makie.hidedecorations!(ax)
 
-    x, y, states = CharacterHistories.coordinates(tree)
+    ## horizontal lines
+    x, y, states = CharacterHistories.coordinates_horizontal(tree)
 
-    c_states = String[]
-    points = Makie.Point2{Float64}[]
+    c_states_horizontal = String[]
+    points_horizontal = Makie.Point2{Float64}[]
     for index in keys(x)
         for i in 2:length(x[index])
             from = Makie.Point(x[index][i-1], y[index])
             to   = Makie.Point(x[index][i],   y[index])
         
-            push!(points, from)
-            push!(points, to)
-            push!(c_states, states[index][i-1])
+            push!(points_horizontal, from)
+            push!(points_horizontal, to)
+            push!(c_states_horizontal, states[index][i-1])
         end
     end
+
+    ## vertical lines
+    x, y, states = CharacterHistories.coordinates_vertical(tree)
+
+    c_states_vertical = String[]
+    points_vertical = Makie.Point2{Float64}[]
+    for index in keys(x)
+        #for i in 2:length(x[index])
+            from = Makie.Point(x[index], y[index][1])
+            to   = Makie.Point(x[index], y[index][2])
+        
+            push!(points_vertical, from)
+            push!(points_vertical, to)
+            push!(c_states_vertical, states[index])
+        #end
+    end
     
-    #state_space = unique(c_states)
-    #state_space = model.state_space
+    points = vcat(points_horizontal, points_vertical)
+    c_states = vcat(c_states_horizontal, c_states_vertical)
+
     state_space = unique(values(CharacterHistories.tipstates(tree)))
 
     tbl = Dict(x => i for (i,x) in enumerate(state_space))
@@ -42,4 +61,4 @@ function CharacterHistories.treeplot(
 end
 
 
-end
+end ## end module extension
--- src/plot.jl
+++ src/plot.jl
@@ -3,7 +3,7 @@ export treeplot
 
 function treeplot() end
 
-function coordinates(tree::Root)
+function coordinates_horizontal(tree::Root)
     n_edges = number_of_edges(tree)
 
     y = zeros(n_edges)
@@ -19,6 +19,12 @@ function coordinates(tree::Root)
     return(x, y, states)
 end
 
+
+function find_y!(node::Tip, y::Vector{Float64})
+    index = node.inbounds.index  ## parental branch index 
+    y[index] = node.index
+end
+
 function find_y!(node::Node, y::Vector{Float64})
     left_branch = node.left
     left_node = left_branch.outbounds
@@ -29,15 +35,52 @@ function find_y!(node::Node, y::Vector{Float64})
     find_y!(left_node, y)
     find_y!(right_node, y)
 
-    index = node.inbounds.index ## parental branch index 
+    #index = node.index
+    index = node.inbounds.index
+
     y[index] = (y[left_branch.index] + y[right_branch.index]) / 2
 end
 
-function find_y!(node::Tip, y::Vector{Float64})
-    index = node.inbounds.index  ## parental branch index 
-    
-    y[index] = node.index
+function coordinates_vertical(tree::Root)
+
+    nd = node_depths(tree)
+    root_height = maximum(nd)
+
+    y = Dict{Int64, Vector{Float64}}()
+    x = Dict{Int64, Float64}()
+    states = Dict{Int64, String}()
+
+    y_middle = find_vertical!(tree, x, y, states, root_height)
+
+    return(x, y, states)
 end
+
+function find_vertical!(node::Tip, x, y, states, t)
+    return(node.index)
+end
+
+function find_vertical!(node::T, x, y, states, t) where {T <: InternalNode}
+    left_branch = node.left
+    left_bl = sum(left_branch.times)
+    left_node = left_branch.outbounds
+
+    right_branch = node.right
+    right_bl = sum(right_branch.times)
+    right_node = right_branch.outbounds
+
+    y_left = find_vertical!(left_node, x, y, states, t - left_bl)
+    y_right = find_vertical!(right_node, x, y, states, t - right_bl)
+
+    index = node.index
+
+    y[index] = [y_left, y_right]
+    x[index] = t
+    states[index] = left_branch.states[end]
+
+    y_middle = (y_left + y_right) / 2
+    return(y_middle)
+end
+
 
 function find_x!(
     node::T,
